3ce747386c622f9c0072133ee94487350746cbbd
davidarturolr <da.leon@uniandes.edu.co>
2018-11-01 10:39:02 -0500

    clases Carrito de compras

---
+++ src/main/java/uniandes/isis2304/superAndes/negocio/CarritoDeCompras.java
@@ -0,0 +1,55 @@
+
+
+package uniandes.isis2304.superAndes.negocio;
+
+
+public class CarritoDeCompras implements VOCarritoDeCompras
+{/* ****************************************************************
+	 * 			Atributos
+	 *****************************************************************/
+	
+	/**
+	 * usado es 0 si no esta en uso
+	 */
+	private int usado;
+	
+
+
+	/* ****************************************************************
+	 * 			Métodos
+	 *****************************************************************/
+	/**
+	 * Constructor por defecto
+	 */
+	public CarritoDeCompras() 
+	{
+		this.usado = 0;
+	}
+
+	public CarritoDeCompras( int usado) 
+	{
+		this.usado = 0;
+	}
+
+	public int getUsado() 
+	{
+		return usado;
+	}
+
+	public void setUsado(int usado) 
+	{
+		this.usado = usado;
+	}
+	
+
+
+	@Override
+	public String toString() 
+	{
+		String resp = "CarritoDeCompras [usado=" + usado +  "]";
+		
+		return resp;
+		
+	}
+
+}
